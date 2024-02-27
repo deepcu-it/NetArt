d8583ebab36a1a84c19c268a2d01a400f56049ea
Deep <deepghosal445@gmail.com>
2024-02-27 16:13:25 +0530

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header=() =>{
     return (
-        <img className="header" src="../Netart-Test/asserts/logo.png" alt="logo" />
+        <img className="header" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDeBOwIKbP9Spb53pe8pjpGkId0ecJfrgnlw&usqp=CAU" alt="logo" />
     );
 }
 
