5219208ffe198ac4cd970dd9f61ca0b9bba80a2c
Deep <deepghosal445@gmail.com>
2024-02-27 14:53:05 +0530

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,5 @@ import react from '@vitejs/plugin-react';
 
 export default {
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['@mui/icons-material/Call', '@mui/icons-material/FacebookRounded', '@mui/icons-material/Language']
-    }
-  }
+  
 }
